eea05f8116c367a24ee0d189ff4231e0989a0cc7
Korotkova Julia <korotkovayuliav@gmail.com>
2022-09-13 13:00:15 +0200

    done

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        float division = toBeDivided / 5f;
+        System.out.println(division);
+    }
+
+    public static void main(String[] args) {
+        NumberDivider numberDivider = new NumberDivider();
+        numberDivider.divide(14);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        float seconds = 60 * minutes;
+        System.out.println(seconds);
+    }
+
+    public static void main(String[] args) {
+        TimeConvertor timeConvertor = new TimeConvertor();
+        timeConvertor.convert(10);
     }
 }
